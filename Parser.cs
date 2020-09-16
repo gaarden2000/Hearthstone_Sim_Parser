a7a7f9cf6556e94362b5c7b9a9c78e43eb85e15b
Sander Gaarden <sandergaarden@gmail.com>
2020-09-16 10:39:21 +0200

    Initial port to Git.

---
+++ Parser.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hearthstone_Sim_Parser
+{
+    class Parser
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hearthstone_Sim_Parser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
