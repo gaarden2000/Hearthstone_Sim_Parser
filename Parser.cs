942c036d909163558961773f2bd6d645efe2c8d8
Sander Gaarden <sandergaarden@gmail.com>
2020-10-12 16:20:40 +0200

    no message

--- Parser.cs
+++ Parser.cs
@@ -15,17 +15,12 @@ namespace Hearthstone_Sim_Parser
          */
         public static string[] ReadFromCSV(string set, string path)
         {
-            // TODO
-            // Find amount of cards to curate
-            /// lineAmount
-            /// Write correct cards
-
             int lineAmount = FindLineAmount(set, path); // Function finds the amount of lines necessary in the array
             Console.WriteLine("Amount of lines found for writing found: " + lineAmount);
 
             string[] CSVArray = new string[lineAmount]; // Array meant for writing to a new file with the wanted set
 
-            CSVArray = PopulateCSVArray(CSVArray, path);
+            CSVArray = PopulateCSVArray(CSVArray, set, path);
 
             return CSVArray;
         }
@@ -52,13 +47,13 @@ namespace Hearthstone_Sim_Parser
             return Indices;
         }
 
+
         /*
          * Function: FindLineAmount
          * Input: The set (as string) that is looked for in the file, the path (as string) to the file
          * Output: An integer of amount of lines to write
          * Remarks: None
          */
-
         public static int FindLineAmount(string set, string path)
         {
             int lineAmount = 1; // This variable is set to 1 in order to count the header at the top of the document
@@ -68,7 +63,7 @@ namespace Hearthstone_Sim_Parser
                 string line; // String variable used to store the read line
                 string cardTrue = "True"; // String to check for collectible flag of the card
 
-                while((line = sr.ReadLine()) != null) // Readline() goes to the next line of the file after being called
+                while((line = sr.ReadLine()) != null) // Readline() goes to the next line of the file after being called. While loop breaks when ReadLine() returns null
                 {
                     int[] Indices = FindIndex(',', line); // Array of indices of commas of the given line
 
@@ -85,21 +80,40 @@ namespace Hearthstone_Sim_Parser
             return lineAmount;
         }
 
-        public static string[] PopulateCSVArray(string[] CSVArray, string path)
+        
+        /*
+         * Function: PopulateCSVArray
+         * Input: The string array to write to, the set (as string) that is looked for in the file, the path (as string) to the file
+         * Output: The array containing the correct lines from the CSV file
+         * Remarks: None
+         */
+        public static string[] PopulateCSVArray(string[] CSVArray, string set, string path)
         {
 
             using (StreamReader sr = new StreamReader(path)) // Reads the file given in path
             {
                 CSVArray[0] = sr.ReadLine(); // First line of the file is the header, which should be included for neatness. StreamReader object remembers how many lines has been read, going down one line with every ReadLine()
+                int arrayIndex = 1; // Integer to put a new line in a new index. Set to 1 because of the header
+
                 string line; // String variable used to store the read line
                 string cardTrue = "True"; // String to check for collectible flag of the card
 
+                while((line = sr.ReadLine()) != null) // Readline() goes to the next line of the file after being called. While loop breaks when ReadLine() returns null
+                {
+                    int[] Indices = FindIndex(',', line);
 
-
+                    // This if statement is true if the following is true:
+                    // The set read in the line is the same as the one given (set is the same as the set written in the line)
+                    // The card is a collectible card (cardTrue is the same as the flag in the line)
+                    if (set.Equals(line.Substring(Indices[1] + 1, 7)) && cardTrue.Equals(line.Substring(Indices[10] + 1, 4)))
+                    {
+                        Console.Write(line); // Written for clarity purposes
+                        CSVArray[arrayIndex] = line; // Line is added to the array
+                        Console.WriteLine(" arrayIndex of line: " + arrayIndex); // Written for clarity purposes
+                        arrayIndex++; // Increments arrayIndex so the next line to add will be on a new index
+                    }
+                }
             }
-
-
-
             return CSVArray;
         }
 
